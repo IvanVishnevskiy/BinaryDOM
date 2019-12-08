cd7d6d52d106ada2d426ba70d44fa7a2d07169fd
forgetable <io@forgetable.ru>
2019-12-08 18:09:03 +0300

    Component

--- vdom/domschema.js
+++ vdom/domschema.js
@@ -1,5 +1,6 @@
 export default `
 attribute#0000 name:string value:string
-HTMLNode#0002 tag:int16 attrs:Array<attribute> nodes:Array<HTMLNode|TextNode>
+HTMLNode#0002 tag:int16 attrs:Array<attribute> nodes:Array<HTMLNode|TextNode|Component>
 TextNode#0003 text:string
+Component#0004 id:int64 attrs<attribute> node:string
 `
